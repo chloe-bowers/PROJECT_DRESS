687dca234843d32bc110667cc50fc01b7d7bfecb
Erick Gonzalez Gonzalez <gonhoff@hotmail.com>
2022-11-22 17:28:43 +0100

    cloudinary

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2022_11_22_122919) do
-=======
-
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_131412) do
-
->>>>>>> 862673bef1e4405ab0431293603fa58d333486e1
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
