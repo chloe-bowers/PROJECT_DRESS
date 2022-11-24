399d0997567fcd935db5aed46f51a8a656df0170
Nicola Holtappels <nicola.holtappels@gmail.com>
2022-11-24 12:11:36 +0100

    search

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,15 @@
 class OffersController < ApplicationController
   def index
+    if params[:query].present?
+      # sql_query = <<~SQL
+      #   movies.title @@ :query
+      #   OR movies.synopsis @@ :query
+      #   OR directors.first_name @@ :query
+      #   OR directors.last_name @@ :query
+      # SQL
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+      @offers = Offer.global_search(params[:query])
+    else
     @offers = policy_scope(Offer)
   end
 
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,4 +4,13 @@ class Offer < ApplicationRecord
   has_many_attached :photos
   validates :title, :size, :price_per_day, presence: true
   validates :title, uniqueness: true
+
+  pg_search_scope :global_search,
+    against: %i[ title size price_per_day condition description ],
+    associated_against: {
+      user: %i[first_name last_name]
+    },
+    using: {
+      tsearch: { prefix: true }
+    }
 end
